840bbd5656a7442d5e8527aa6e647679f4b5bf2c
KatudnakJ <katudnak_j@su.ac.th>
2026-04-04 11:43:00 +0700

    [KAN-97] Update required fields @Notnull and @NotBlank also @Positive for BigDemical and integer

--- src/main/java/com/senior/candleShopProject/feature/product/controller/dto/request/UpdateProductReq.java
+++ src/main/java/com/senior/candleShopProject/feature/product/controller/dto/request/UpdateProductReq.java
@@ -2,6 +2,9 @@ package com.senior.candleShopProject.feature.product.controller.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -15,27 +18,35 @@ public class UpdateProductReq {
 
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "Candle")
         @JsonProperty("product_name")
+        @NotNull
+        @NotBlank
         private String productName;
 
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "19.99")
         @JsonProperty("price")
+        @NotNull
+        @Positive
         private BigDecimal price;
 
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "0.5")
         @JsonProperty("weight")
+        @NotNull
+        @Positive
         private double weight;
 
-        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "A scented candle with a relaxing aroma.")
+        @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED, example = "A scented candle with a relaxing aroma.")
         @JsonProperty("description")
         private String description;
 
-        @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED,
                 example = "true", defaultValue = "true")
         @JsonProperty("active")
+        @NotNull
         private boolean active =true;
 
         @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "false", defaultValue = "false")
         @JsonProperty("featured")
+        @NotNull
         private boolean featured = false;
 
         @Schema(requiredMode = Schema.RequiredMode.NOT_REQUIRED)
